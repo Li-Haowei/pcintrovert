f605c5fe548241a41a7a792eca720d63f8fd56d8
Haowei Li <91715036+Li-Haowei@users.noreply.github.com>
2022-11-26 20:32:42 -0500

    some demos

--- src/components/buildPC/BuildPC.js
+++ src/components/buildPC/BuildPC.js
@@ -10,6 +10,9 @@ import axios from 'axios';
     Cases: name: AMANSON PC CASE ATX Mid Tower Case, img: https://m.media-amazon.com/images/I/81JwnDYiKTL._AC_SL1500_.jpg, price: $88.99
            name: GIM ATX Mid-Tower Case White Gaming PC Case, img: https://m.media-amazon.com/images/I/71vbA44y12L._AC_SL1500_.jpg, price: $127.99
            name: Corsair 4000D Airflow Tempered Glass Mid-Tower ATX PC Case - Black, img: https://m.media-amazon.com/images/I/81hL4tPkXZL._AC_SL1500_.jpg, price: $89.99
+    Cooling: name: Enermax Liqmax III 240 RGB AIO CPU Liquid Cooler, img: https://m.media-amazon.com/images/I/61fJ8nGgqPL._SL1500_.jpg, price: $54.99
+             name: Cooler Master Hyper 212 Black Edition RGB CPU Air Cooler, img:https://m.media-amazon.com/images/I/81gIOOqmS7L._AC_SL1500_.jpg, price: $50.53
+             name: Corsair iCUE H150i Elite Capellix Liquid CPU Cooler, img: https://m.media-amazon.com/images/I/51e41U32IWL._AC_SL1000_.jpg, price: $179.99
 
 
     
@@ -37,7 +40,12 @@ class BuildPC extends React.Component{
         document.getElementById("wrapper").classList.remove('active');
     }
 
+    cleanShelf = () => {
+        document.getElementsByClassName('products')[0].innerHTML = "";
+    }
+
     loadCasesToProducts = () => {
+        this.cleanShelf();
         let shelf = document.getElementsByClassName('products')[0];
         /* create a grid for each case */
         shelf.innerHTML += `
@@ -64,6 +72,58 @@ class BuildPC extends React.Component{
                 <div class="product-info">
                     <p class="product-name">Corsair 4000D Airflow Tempered Glass Mid-Tower ATX PC Case - Black</p>
                     <p class="product-price">$89.99</p>
+                </div>
+            </div>
+            `
+        shelf.innerHTML += `
+            <div class="grid">
+                <img src="https://m.media-amazon.com/images/I/71eWq2GbhhL._AC_SL1500_.jpg" alt="case">
+                <div class="product-info">
+                    <p class="product-name">HYTE Y60 Modern Aesthetic Dual Chamber Panoramic Tempered Glass Mid-Tower ATX Computer Gaming Case</p>
+                    <p class="product-price">$159.99</p>
+                </div>
+            </div>
+            `
+        this.addExpandableFunctions();
+    }
+
+    loadCoolingToProducts = () => {
+        this.cleanShelf();
+        let shelf = document.getElementsByClassName('products')[0];
+        /* create a grid for each case */
+        shelf.innerHTML += `
+            <div class="grid">
+                <img src="https://m.media-amazon.com/images/I/61fJ8nGgqPL._SL1500_.jpg" alt="case">
+                <div class="product-info">
+                    <p class="product-name">Enermax Liqmax III 240 RGB AIO CPU Liquid Cooler</p>
+                    <p class="product-price">$54.99</p>
+                </div>
+            </div>
+            `
+        shelf.innerHTML += `
+            <div class="grid">
+                <img src="https://m.media-amazon.com/images/I/81gIOOqmS7L._AC_SL1500_.jpg" alt="case">
+                <div class="product-info">
+                    <p class="product-name">Cooler Master Hyper 212 Black Edition RGB CPU Air Cooler</p>
+                    <p class="product-price">$50.53</p>
+                </div>
+            </div>
+            `
+        shelf.innerHTML += `
+            <div class="grid">
+                <img src="https://m.media-amazon.com/images/I/71Q10b2FtBL._AC_SL1500_.jpg" alt="case">
+                <div class="product-info">
+                    <p class="product-name">Thermaltake UX200 SE 5V Motherboard</p>
+                    <p class="product-price">$29.99</p>
+                </div>
+            </div>
+            `
+        shelf.innerHTML += `
+            <div class="grid">
+                <img src="https://m.media-amazon.com/images/I/71irWztV+qL._AC_SL1500_.jpg" alt="case">
+                <div class="product-info">
+                    <p class="product-name">Jonsbo CR1400 RGB CPU Air Cooler</p>
+                    <p class="product-price">$25.99</p>
                 </div>
             </div>
             `
@@ -99,7 +159,7 @@ class BuildPC extends React.Component{
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" onClick={this.loadCoolingToProducts}>
                         Cooling
                         <span className="pull-right"></span>
                         </a>
