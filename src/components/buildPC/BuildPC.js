9c07fe3e851832c8657ae0c9d78dc492ac12aefa
Haowei Li <91715036+Li-Haowei@users.noreply.github.com>
2022-11-26 20:10:38 -0500

    progress

--- src/components/buildPC/BuildPC.js
+++ src/components/buildPC/BuildPC.js
@@ -5,6 +5,15 @@ import './BuildPC.css';
 import { useState, useEffect } from 'react';
 import menuIcon from '../../assets/menu.png';
 import axios from 'axios'; 
+/*
+    Products:
+    Cases: name: AMANSON PC CASE ATX Mid Tower Case, img: https://m.media-amazon.com/images/I/81JwnDYiKTL._AC_SL1500_.jpg, price: $88.99
+           name: GIM ATX Mid-Tower Case White Gaming PC Case, img: https://m.media-amazon.com/images/I/71vbA44y12L._AC_SL1500_.jpg, price: $127.99
+           name: Corsair 4000D Airflow Tempered Glass Mid-Tower ATX PC Case - Black, img: https://m.media-amazon.com/images/I/81hL4tPkXZL._AC_SL1500_.jpg, price: $89.99
+
+
+    
+*/
 class BuildPC extends React.Component{
     toggleSidebar = () => {
         document.getElementById("sidebar").classList.toggle('active');
@@ -28,76 +37,54 @@ class BuildPC extends React.Component{
         document.getElementById("wrapper").classList.remove('active');
     }
 
-    // get all information from https://en.wikipedia.org/wiki/List_of_computer_hardware_manufacturers
-    getComponents = () => {
-        axios.get('https://en.wikipedia.org/w/api.php?action=parse&format=json&page=List_of_computer_hardware_manufacturers&origin=*')
-        .then(response => {
-            //parse all the data and put them into the state
-            let data = response.data;
-            console.log(data.parse)
+    loadCasesToProducts = () => {
+        let shelf = document.getElementsByClassName('products')[0];
+        /* create a grid for each case */
+        shelf.innerHTML += `
+            <div class="grid">
+                <img src="https://m.media-amazon.com/images/I/81JwnDYiKTL._AC_SL1500_.jpg" alt="case">
+                <div class="product-info">
+                    <p class="product-name">AMANSON PC CASE ATX Mid Tower Case</p>
+                    <p class="product-price">$88.99</p>
+                </div>
+            </div>
+            `
+        shelf.innerHTML += `
+            <div class="grid">
+                <img src="https://m.media-amazon.com/images/I/71vbA44y12L._AC_SL1500_.jpg" alt="case">
+                <div class="product-info">
+                    <p class="product-name">GIM ATX Mid-Tower Case White Gaming PC Case</p>
+                    <p class="product-price">$127.99</p>
+                </div>
+            </div>
+            `
+        shelf.innerHTML += `
+            <div class="grid">
+                <img src="https://m.media-amazon.com/images/I/81hL4tPkXZL._AC_SL1500_.jpg" alt="case">
+                <div class="product-info">
+                    <p class="product-name">Corsair 4000D Airflow Tempered Glass Mid-Tower ATX PC Case - Black</p>
+                    <p class="product-price">$89.99</p>
+                </div>
+            </div>
+            `
+        this.addExpandableFunctions();
+    }
 
-            // get all the cases
-            let cases = [];
-            let caseData = data.parse.text['*'].split('Cases')[1].split('Cooling')[0];
-            let caseDataArray = caseData.split('<li>');
-            for (let i = 1; i < caseDataArray.length; i++){
-                let caseName = caseDataArray[i].split('</li>')[0];
-                cases.push(caseName);
-            }
-
-            // get all the motherboards
-            let motherboards = [];
-            let motherboardData = data.parse.text['*'].split('Motherboards')[1].split('Power supplies')[0];
-            let motherboardDataArray = motherboardData.split('<li>');
-            for (let i = 1; i < motherboardDataArray.length; i++){
-                let motherboardName = motherboardDataArray[i].split('</li>')[0];
-                motherboards.push(motherboardName);
-            }
-
-            // get all the CPUs
-            let cpus = [];
-            let cpuData = data.parse.text['*'].split('CPUs')[1].split('GPUs')[0];
-            let cpuDataArray = cpuData.split('<li>');
-            for (let i = 1; i < cpuDataArray.length; i++){
-                let cpuName = cpuDataArray[i].split('</li>')[0];
-                cpus.push(cpuName);
-            }
-
-            // get all the GPUs
-            let gpus = [];
-            let gpuData = data.parse.text['*'].split('GPUs')[1].split('Memory')[0];
-            let gpuDataArray = gpuData.split('<li>');
-            for (let i = 1; i < gpuDataArray.length; i++){
-                let gpuName = gpuDataArray[i].split('</li>')[0];
-                gpus.push(gpuName);
-            }
-
-            // get all the memory
-            let memories = [];
-            let memoryData = data.parse.text['*'].split('Memory')[1].split('Storage')[0];
-            let memoryDataArray = memoryData.split('<li>');
-            for (let i = 1; i < memoryDataArray.length; i++){
-                let memoryName = memoryDataArray[i].split('</li>')[0];
-                memories.push(memoryName);
-            }
-
-            // get all the storage
-            let storages = [];
-            let storageData = data.parse.text['*'].split('Storage')[1].split('Power supplies')[0];
-            let storageDataArray = storageData.split('<li>');
-            for (let i = 1; i < storageDataArray.length; i++){
-                let storageName = storageDataArray[i].split('</li>')[0];
-                storages.push(storageName);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    addExpandableFunctions = () => {
+        //when one expands, all others collapse
+        let grids = document.getElementsByClassName('grid');
+        for (let i = 0; i < grids.length; i++){
+            grids[i].addEventListener('click', () => {
+                for (let j = 0; j < grids.length; j++){
+                    grids[j].classList.remove('expand');
+                }
+                grids[i].classList.toggle('expand');
+            })
+        }
     }
 
 
     render(){
-        this.getComponents();
         return (
             <div className="buildPc">
 
@@ -106,7 +93,7 @@ class BuildPC extends React.Component{
             <div id="sidebar"> 
                 <ul>
                     <li>
-                        <a href="#">
+                        <a href="#" onClick={this.loadCasesToProducts}>
                         Case
                         <span className="pull-right"></span>
                         </a>
@@ -168,9 +155,8 @@ class BuildPC extends React.Component{
                 </ul>
             </div>
             <div id="wrapperContent">
-                <p className="well lead">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam tempora facere, cum error, tempore sequi. Saepe error alias reprehenderit at blanditiis voluptas quaerat sed. 
-                </p> 
+                <div className='products four-by-four-grids'>
+                </div>
             </div>
 
             </div>
